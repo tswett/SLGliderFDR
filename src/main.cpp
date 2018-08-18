a3719acaad406ed25cafce8647eaba6d04122e34
Tanner Swett <tannerswett@gmail.com>
2018-08-18 16:17:02 -0400

    Basic data recording and reporting

--- src/main.cpp
+++ src/main.cpp
@@ -1,21 +1,29 @@
 #include <Arduino.h>
+#include <EEPROM.h>
 
 #include "I2Cdev.h"
 #include "MPU6050.h"
 
 #define LED_PIN 13
+#define SAMPLES_TO_STORE 170
+
+struct imuRecord {
+    uint8_t forward;
+    uint8_t right;
+    uint8_t up;
+    uint8_t rollRight;
+    uint8_t pitchUp;
+    uint8_t yawRight;
+};
 
 MPU6050 imu;
-
-int16_t forward, right, up;
-int16_t rollright, pitchup, yawleft;
 
 void setup() {
     Serial.begin(9600);
 
     Wire.begin();
     imu.initialize();
-    if (imu.testConnection())
+    if (!imu.testConnection())
         Serial.println("Failed to connect to IMU!");
 
     // Set the digital low-pass filter mode to 5 (meaning a filter bandwidth of 10 Hz).
@@ -24,19 +32,42 @@ void setup() {
     pinMode(LED_PIN, OUTPUT);
 
     Serial.println("fwd\tright\tup\tR-roll\tU-pitch\tR-yaw");
+
+    for (int readSampleIndex = 0; readSampleIndex < SAMPLES_TO_STORE; readSampleIndex++) {
+        Serial.print(EEPROM.read(6 * readSampleIndex) - 128);     Serial.print("\t");
+        Serial.print(EEPROM.read(6 * readSampleIndex + 1) - 128); Serial.print("\t");
+        Serial.print(EEPROM.read(6 * readSampleIndex + 2) - 128); Serial.print("\t");
+        Serial.print(EEPROM.read(6 * readSampleIndex + 3) - 128); Serial.print("\t");
+        Serial.print(EEPROM.read(6 * readSampleIndex + 4) - 128); Serial.print("\t");
+        Serial.print(EEPROM.read(6 * readSampleIndex + 5) - 128); Serial.print("\n");
+    }
 }
 
+int16_t forward, right, up;
+int16_t rollRight, pitchUp, yawLeft;
+
+uint16_t sampleIndex = 0;
+
 void loop() {
-    imu.getMotion6(&right, &forward, &up, &pitchup, &rollright, &yawleft);
+    imu.getMotion6(&right, &forward, &up, &pitchUp, &rollRight, &yawLeft);
 
-    digitalWrite(LED_PIN, yawleft > 0);
+    // digitalWrite(LED_PIN, yawLeft > 0);
 
-    Serial.print(forward);   Serial.print("\t");
-    Serial.print(right);     Serial.print("\t");
-    Serial.print(up);        Serial.print("\t");
-    Serial.print(rollright); Serial.print("\t");
-    Serial.print(pitchup);   Serial.print("\t");
-    Serial.print(-yawleft);  Serial.print("\n");
+    if (sampleIndex >= SAMPLES_TO_STORE) {
+        digitalWrite(LED_PIN, 0);
+    }
+    else {
+        EEPROM.write(6 * sampleIndex,     forward   / 256  + 128);
+        EEPROM.write(6 * sampleIndex + 1, right     / 256  + 128);
+        EEPROM.write(6 * sampleIndex + 2, up        / 256  + 128);
+        EEPROM.write(6 * sampleIndex + 3, rollRight / 256  + 128);
+        EEPROM.write(6 * sampleIndex + 4, pitchUp   / 256  + 128);
+        EEPROM.write(6 * sampleIndex + 5, -(yawLeft / 256) + 128);
+
+        digitalWrite(LED_PIN, sampleIndex % 2);
+
+        sampleIndex++;
+    }
 
     delay(100);
 }
